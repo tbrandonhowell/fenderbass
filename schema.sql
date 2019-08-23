7796bcdff69c33b90eb33e1084287ab1be7309cb
Brandon H <45020672+tbrandonhowell@users.noreply.github.com>
2019-08-22 22:10:24 -0400

    early

---
+++ schema.sql
@@ -0,0 +1,10 @@
+create database fender_basses_db;
+use fender_basses_db;
+create table basses (
+    /* ID setup */
+    id int auto_increment not null;
+    primary key (id);
+    /* The Fields */
+    
+
+)
